78107edf61a99af49d619d4fd6c47713d4e99dc8
vinicius.souza <vinicius.souza@hightechcursos.com.br>
2016-10-06 19:23:46 -0400

    CRUD Completo da Aula 4

--- src/main/java/br/com/fabricadeprogramador/controller/ClienteController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/ClienteController.java
@@ -1,6 +1,9 @@
 package br.com.fabricadeprogramador.controller;
 
-import javax.faces.view.ViewScoped;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,39 @@ public class ClienteController {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	private List<Cliente> clientes;
+	
 	private Cliente cliente =  new Cliente();
+
+	private boolean modoEdicao=false;
 	
-	
+	@PostConstruct
+	public void init() {
+		setClientes(clienteRepository.buscarTodos());
+	}
 	public void salvar(){
 		
-		clienteRepository.save(getCliente());
+		clienteRepository.save(cliente);
+		if(!isModoEdicao())
+			clientes.add(cliente);
+		cliente = new Cliente();
+		setModoEdicao(false);
 	}
 
+	public void excluir(Cliente cliente){
+		clienteRepository.delete(cliente);
+		clientes.remove(cliente);
+	}
+	
+	public void editar(Cliente cliente){
+		setCliente(cliente);
+		setModoEdicao(true);
+	}
+	
+	public void cancelar(){
+		cliente = new Cliente();
+		setModoEdicao(false);
+	}
 
 	public Cliente getCliente() {
 		return cliente;
@@ -31,5 +59,17 @@ public class ClienteController {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	public boolean isModoEdicao() {
+		return modoEdicao;
+	}
+	public void setModoEdicao(boolean modoEdicao) {
+		this.modoEdicao = modoEdicao;
 	}
 }
